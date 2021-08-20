a5a5cbfc9a8e35a684b936a7c29cbd7789cbb0e9
slittyofficials <87329893+slittyofficials@users.noreply.github.com>
2021-08-20 15:31:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const fs = require('fs')
 const Discord = require('discord.js')
-const path = require('path')
 
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES']})
+
+var prefix = '!'
 
 client.on('ready', () => {
 	client.user.setActivity({name:"2021", type:"PLAYING"})
 })
 
 client.on('messageCreate', (message) => {
-	
+	if (message.content == `${prefix}embed`){
+		const Embed = new Discord.MessageEmbed()
+		.setColor("FFFFFF")
+		.setTitle("Example")
+		.setDescription("Embed")
+		.setFooter("Discord.js @v13")
+		message.channel.send({ embeds: [Embed] })
+	}
 })
 
-client.login('ODYzNTAzMDk2MTY4MTg1ODU3.YOn17Q.p7WPKoM9ogFQrwauPpgFmJQMRBU')
+client.login('TOKEN')
